f36af0661f21aba4b4de3c6e214f67083ed8125d
Eliška Tomášková <elatomaskova11@gmail.com>
2022-11-04 13:42:04 +0100

    Novější verze

--- du1_eliska_tomaskova/du1.py
+++ du1_eliska_tomaskova/du1.py
@@ -1,8 +1,10 @@
 import turtle
 
-# Definování velikosti mřížky, strany a zrychlení vykreslování
-A = 3
-B = 3
+# Definování velikosti hrací plochy uživatelem
+A = int(input("Zadej počet sloupců hrací plochy: "))
+B = int(input("Zadej počet řádků hrací plochy: "))
+
+# Definování strany a zrychlení vykreslování
 strana = 100
 turtle.speed(100)
 
@@ -20,47 +22,24 @@ for y in range(B):
 	turtle.right(90)
 turtle.penup()
 
-# Definice vykreslení kolečka
-def kolecko():
-    print("Na řadě je první hráč. Váším symbolem je kolečko.")
-    
-    # Pokud uživatel zadá jiné souřadnice než 1 až 3, je vyzýván, dokud je nezadá správně
-    while True:
-        x1 = float(input("Zadejte souřadnice x: "))
-        y1 = float(input("Zadejte souřadnice y: "))
-        if ((x1 in (1,2,3)) and (y1 in (1,2,3))):
-            break 
-        else:
-            print("Zadali jste nesprávné souřadnice. Zkuste to znovu.")
-
-    # Želva se přemístí na souřadnice zadané uživatelem
-    # Předpisy ve fuknci setpos převádí souřadnice zadané uživatelem na skutečné souřadnice
-    # Mřížka je definována tak, že políčko [1,1] je v levém horním rohu
-    turtle.setpos(strana*x1-strana/2,-strana*y1+3.1*strana)  
-    turtle.setheading(0)
-    turtle.pendown()
-    turtle.circle(40) 
-    turtle.penup()
-
 # Definice vykreslení křížku
 def krizek ():
-    print("Na řadě je duhý hráč. Váším symbolem je křížek.")
+    print("Na řadě je první hráč. Váším symbolem je křížek.")
 
-    # To stejné jak pro kolečko platí i pro křížek   
+    # Pokud uživatel zadá souřadnice mimo hrací plochu, je vyzýván, dokud nezadá existující souřadnice
     while True:
         x2 = float(input("Zadejte souřadnice x: "))
         y2 = float(input("Zadejte souřadnice y: "))
-        if ((x2 in (1,2,3)) and (y2 in (1,2,3))):
+        if (( A >= x2 >= 1) and ( B >= y2 >= 1) ):
             break 
         else:
             print("Zadali jste nesprávné souřadnice. Zkuste to znovu.")
     
-    # Želva se přemístí na střed daného čtverce
-    turtle.setpos(strana*x2-strana/2,-strana*y2+7/2*strana)
+    turtle.setpos(strana*x2-1/2*strana,strana*(B-y2)+1/2*strana)  # Želva se přemístí na střed daného čtverce
     turtle.setheading(0)
     turtle.pendown()
 
-    # Vykreslení křížku ze středu čtverce
+    # Vykreslení křížku ze středu políčka
     turtle.right(45)
     turtle.forward(50)
     turtle.left(180)
@@ -73,16 +52,35 @@ def krizek ():
     turtle.forward(100)
     turtle.penup()
 
-# Jednotliví hráči se střídají, každý má čtyři tahy
-for a in range (4):
-    kolecko()
-    krizek()
-
-# Mřížka má 9 polí, končí tedy hráč, který začínal
-kolecko()
+# Definice vykreslení kolečka
+def kolecko():
+    print("Na řadě je druhý hráč. Váším symbolem je kolečko.")
     
+    # Pokud uživatel zadá souřadnice mimo hrací plochu, je vyzýván, dokud nezadá existující souřadnice
+    while True:
+        x1 = float(input("Zadejte souřadnice x: "))
+        y1 = float(input("Zadejte souřadnice y: "))
+        if (( A >= x1 >= 1) and ( B >= y1 >= 1) ):
+            break 
+        else:
+            print("Zadali jste nesprávné souřadnice. Zkuste to znovu.")
+
+    turtle.setpos(strana*x1-1/2*strana,strana*(B-y1)+1/10*strana)  # Želva se přemístí na střed osy x a 1/10 osy y daného čtverce
+    turtle.setheading(0)
+    turtle.pendown()
+    turtle.circle(40) 
+    turtle.penup()
+
+# Jednotliví hráčí se střídají -> hra skončí až dojdou všechna políčka
+for a in range (A*B//2):
+    krizek()
+    kolecko()
+
+if A*B % 2 == 1: # Pokud je políček lichý počet, hraje ještě jednou první hráč
+    krizek()
+   
 print("Hra skončila.")
 turtle.exitonclick()
    
 
-
+   
