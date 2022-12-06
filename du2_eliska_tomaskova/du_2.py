871527cfa277f2177d18663213889bc922ae4917
Eliška Tomášková <elatomaskova11@gmail.com>
2022-12-06 17:59:46 +0100

    další

--- du2_eliska_tomaskova/du_2.py
+++ du2_eliska_tomaskova/du_2.py
@@ -3,7 +3,7 @@ import csv
 
 # Kontrola existence/přístupnosti vstupního souboru
 try:
-    with open("vstupni_data.csv", encoding="utf8") as fcsvinfile,\
+    with open("bum.csv", encoding="utf8") as fcsvinfile,\
             open("vystup_7dni.csv", "w", encoding="utf8") as f,\
                 open("vystup_rok.csv", "w", encoding="utf8") as f:
         reader = csv.reader(f, delimiter = ",")
@@ -16,61 +16,41 @@ except PermissionError:
 
 
 # Sedmidenní průměry
-with open("vstupni_data_carky.csv", encoding="utf-8") as f,\
+with open("bum.csv", encoding="utf-8") as f,\
     open("vystup_7dni.csv", "w", newline='', encoding="utf-8") as fout:
     reader = csv.reader(f, delimiter = ",")
     writer = csv.writer(fout)
 
     # definovani proměnných    
     prutoky = 0               
-    radky = 0                           
+    radky = 0 
+    zbytek = 0                       
     
     # procházeni jednotlivých řádku
     for row in reader:      
         radky += 1
-        prutoky += float(row[-1])                        
+        prutoky += float(row[-1]) 
         
-        if radky % 7 == 1:                              
+                            
+        
+        if radky % 7 == 1:                                   # prvni radek ze sedmi se ulozi do promenne vytiskni    
             vytiskni = row[0:-1]
 
-        if radky % 7 == 0:  
-            
-            prumer_tyden_nezaokrouhlene = round((prutoky/7),4)      
-            prumer_tyden = (f'{prumer_tyden_nezaokrouhlene:.4f}')    
+        if radky  == 7:                                      # pokud narazi na sedmy radek vypise sedmidenni prumer    
+            prumer_tyden = (f'{(prutoky/7):.4f}')   
             vytiskni.append(prumer_tyden)            
             writer.writerow(vytiskni)           
             prutoky = 0
+            radky = 0
 
+    if (radky-1) % 7 != 6:
+        prumer_prutok_tyden = prutoky / radky
+        vytiskni[-1] = (f"{prumer_prutok_tyden:.4f}")
+        writer.writerow(vytiskni)
 
-# Roční průměry
-with open("vstupni_data_carky.csv", encoding="utf-8") as f,\
-    open("vystup_rok.csv", "w", newline='', encoding="utf-8") as fout:
-    reader = csv.reader(f, delimiter = ",")
-    writer = csv.writer(fout)
+  
 
-    # definovani proměnných
-    rok = 0    
-    prutoky_rok = 0               
-    pocet_dni = 0                   
-    
-    # procházeni jednotlivých řádku
-    for row in reader: 
-
-        prutoky_rok += 1
-        pocet_dni += 1                              
-        
-        if rok == 0:                             
-            vytiskni2 = row[0:-1]
-
-        if rok != int(row[2]):
-            prumer_rok_nezaokrouhlene = round((prutoky_rok/pocet_dni),4)      
-            prumer_rok = (f'{prumer_rok_nezaokrouhlene:.4f}')    
-            vytiskni.append(prumer_rok)            
-            writer.writerow(vytiskni)           
-            prutoky_rok = 0
- 
-
-
+print("Úspěch!")
 
 
     
